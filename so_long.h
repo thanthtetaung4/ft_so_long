c5bfd5446f965bd5c82347eef170db6fdc0f9e75
Thant Htet Aung <taung@c2r1s2.42singapore.sg>
2024-10-14 20:07:58 +0800

    render test

---
+++ so_long.h
@@ -0,0 +1,9 @@
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+#include <stdio.h>
+#include <mlx.h>
+
+#define CAT "textures/cat_sprite.xpm"
+#define BBC "textures/big_bad_cat.xpm"
+#endif
--- src/main.c
+++ src/main.c
@@ -1,39 +1,24 @@
-#include <mlx.h>
-#include <stdio.h>
-
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
+#include "../so_long.h"
 
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
-	t_data	img;
+	void	*mlx_img;
+	//char	*img_path = "./wall.xpm";
+	int		img_width;
+	int		img_height;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1080, 720, "Hello world!");
-	img.img = mlx_new_image(mlx, 1080, 720);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	my_mlx_pixel_put(&img, 50, 50, 0x00FFFFFF);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	// mlx_put_image_to_window(mlx, mlx_win, img.img, 1, 0);
-	// mlx_put_image_to_window(mlx, mlx_win, img.img, 2, 0);
-	// mlx_put_image_to_window(mlx, mlx_win, img.img, 3, 0);
-	// mlx_put_image_to_window(mlx, mlx_win, img.img, 4, 0);
+	mlx_win = mlx_new_window(mlx, 500, 500, "so_long");
+	printf("img_width: %i, img_height: %i\n",img_width, img_height);
+	mlx_img = mlx_xpm_file_to_image(mlx, BBC, &img_width, &img_height);
+
+	if(!mlx_img)
+		printf("BAD IMAGE\n");
+	else
+		printf("IMG OK\n");
+	printf("img_width: %i, img_height: %i\n",img_width, img_height);
+	mlx_put_image_to_window(mlx, mlx_win, mlx_img, img_width, img_height);
 	mlx_loop(mlx);
 }
